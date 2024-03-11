209d9d3a4be955c0d27dbbc0b309b4647535c75f
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2024-03-11 23:22:08 +0000

    try out php email form sending

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,3 +1,13 @@
-const sendEmail = function () {
-    
+const sendEmail = function (e) {
+  /*   e.preventDefault();
+    const name = document.querySelector("form #name").value;
+    const email = document.querySelector("form #email").value;
+    const subject = document.querySelector("form #subject").value;
+    const messsage = document.querySelector("form #message").value;
+/*     if (!(emailIsInCorrectFormat(email))) return false;
+ */    /* window.location.href = "#"
+    window.open(``, "_blank") */
+   /*  window.open(`mailto:charlesobird@outlook.com?subject=${name} - ${subject} (${email})&body=${messsage}`,"_blank") */
+    //window.location.assign(`mailto:charlesobird@outlook.com?subject=${name} - ${subject} (${email})&body=${messsage}`)  */
+
 }
