5dba4da21450c2184d9210ee4b541f5b5ec89156
charlesobird <charlesobird+github@gmail.com>
2024-04-19 15:39:42 +0100

    initial commit

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,13 +1,30 @@
-const sendEmail = function (e) {
-  /*   e.preventDefault();
-    const name = document.querySelector("form #name").value;
-    const email = document.querySelector("form #email").value;
-    const subject = document.querySelector("form #subject").value;
-    const messsage = document.querySelector("form #message").value;
-/*     if (!(emailIsInCorrectFormat(email))) return false;
- */    /* window.location.href = "#"
-    window.open(``, "_blank") */
-   /*  window.open(`mailto:charlesobird@outlook.com?subject=${name} - ${subject} (${email})&body=${messsage}`,"_blank") */
-    //window.location.assign(`mailto:charlesobird@outlook.com?subject=${name} - ${subject} (${email})&body=${messsage}`)  */
-
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
 }
+form.addEventListener("submit", handleSubmit)
--- js/mobileNav.js
+++ js/mobileNav.js
@@ -10,18 +10,26 @@ function openNav() {
     const mobileNavItems = navItems.cloneNode(true);
     mobileNavItems.id = "mobileNavItems";
     nav.after(mobileNavItems);
-    mobileNavItems.classList.add("column", "overflow-hidden", MARK_CLASS);
-    mobileNavItems.classList.remove("row");
-    mobileNavItems.classList.replace("row", "column")    
-    mobileNavItems.classList.add("openNav");
-    navToggleIcon.classList.replace(BARS_CLASS, MARK_CLASS);
+    setTimeout(() => {
+        mobileNavItems.classList.add("column");
+        mobileNavItems.classList.remove("row");
+        mobileNavItems.classList.replace("row", "column")    
+        mobileNavItems.classList.add("openNav");
+        document.body.classList.add("overflow-hidden");
+        navToggleIcon.classList.replace(BARS_CLASS, MARK_CLASS);
+        mobileNavItems.style.height = "100vh"
+    }, 25);
 }
 
 function closeNav() {
     if (document.querySelector("#mobileNavItems")) {
         document.querySelector("#mobileNavItems").classList.remove("openNav");
         navToggleIcon.classList.replace(MARK_CLASS, BARS_CLASS);
-        document.body.removeChild(document.querySelector("#mobileNavItems"));
+        document.querySelector("#mobileNavItems").style.height = "0";
+        setTimeout(() => {
+            document.body.removeChild(document.querySelector("#mobileNavItems"))
+        }, 200);
+        document.body.classList.remove("overflow-hidden");
     }
 }
 navToggle.addEventListener("click", (e) => {
